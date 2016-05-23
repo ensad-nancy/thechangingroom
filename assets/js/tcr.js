71af86bb0882b2e27dd7fd35a2f06f9d82c4e97f
dcfvg <qwsx@dcfvg.com>
2016-05-23 11:48:38 +0200

    design update

--- assets/js/tcr.js
+++ assets/js/tcr.js
@@ -1,7 +1,22 @@
 // var templates = getTemplates();
 
+var posts = {};
+var tags = {};
+
+var backgrounds =  [
+  "http://www.ralphlauren.fr/",
+  "http://www.acanthe-paris.com/",
+  "http://www.cyrillus.fr/",
+  "http://www.gap.eu/",
+  "http://www.brooksbrothers.com/"
+]
+
+var manetSevice = 'https://manet.herokuapp.com/?url=';
+var manetParam = '&delay=1000&format=jpg';
+
+
 $.ajax({
-    url: "http://api.tumblr.com/v2/blog/thechangingroom.tumblr.com/posts/",
+    url:"http://api.tumblr.com/v2/blog/thechangingroom.tumblr.com/posts/",
     data: {
       api_key:'Srhk9qkcJO69pAoB4ltM5uIqpwUBO7kgDqDEaCD9Jo8EafWyHE',
       notes_info: true
@@ -9,16 +24,10 @@ $.ajax({
     dataType: 'jsonp',
     success: function(data){
 
+      tags = _(data.response.posts).map('tags').flatten().uniq().value();
+      posts = data.response.posts;
 
-
-      console.log(data.response.posts);
-
-      var tags = _(data.response.posts).map('tags').flatten().uniq().value();
-
-      console.log(tags);
-
-      $('#results').html(plateforme.posts( data.response ) );
-      $('#tags').html(plateforme.tags( {tags:tags} ));
+      locationHashChanged();
 
       $("#results img").addClass('img-responsive');
       $("iframe").addClass('embed-responsive-item').wrap( "<div class='embed-responsive embed-responsive-16by9'></div>" );;
@@ -26,12 +35,30 @@ $.ajax({
 });
 
 
-$(document).mousemove(function(e) {
-    $('.logo').offset({
-        left: e.pageX,
-        top: e.pageY
-    });
-});
+$('.logo').click(function() { $('html,body').animate({scrollTop:0},'slow') });
+
+function locationHashChanged() {
+
+    var tag = location.hash.replace('#','');
+    var postFiltered = _.reject(posts,function(d){ return d.tags.indexOf(tag) < 0 });
+
+    $('#results').html(plateforme.posts( {posts:postFiltered, query:tag} ) );
+    $('#tags').html(plateforme.tags( {tags:tags, query:tag} ));
+
+    var bg = _(backgrounds).shuffle().first();
+
+    console.log(bg);
+
+    $("body").css('background-image', 'url('+manetSevice+encodeURI(bg)+manetParam+')' );
+
+
+}
+
+window.onhashchange = locationHashChanged;
+
+
+
+
 
 Handlebars.registerHelper('debug', function(optionalValue) {
   console.log('Current Context');
@@ -43,4 +70,11 @@ Handlebars.registerHelper('debug', function(optionalValue) {
     console.log('====================');
     console.log(optionalValue);
   }
+});
+
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b)
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
 });
